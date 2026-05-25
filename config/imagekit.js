2c956fa1428a334178d85409c262d81f67a0f081
aman05hub <shiekhaman1@gmail.com>
2026-05-25 13:09:23 +0530

    remove resend

--- config/imagekit.js
+++ config/imagekit.js
@@ -1,14 +1,15 @@
 const ImageKit = require("imagekit");
 require("dotenv").config();
 
+console.log("Public Key:", process.env.IMAGEKIT_PUBLIC_KEY);
+console.log("Private Key:", process.env.IMAGEKIT_PRIVATE_KEY ? "EXISTS" : "MISSING");
+console.log("URL Endpoint:", process.env.IMAGEKIT_URL_ENDPOINT);
+
 const imagekit = new ImageKit({
     publicKey: process.env.IMAGEKIT_PUBLIC_KEY,
     privateKey: process.env.IMAGEKIT_PRIVATE_KEY,
     urlEndpoint: process.env.IMAGEKIT_URL_ENDPOINT
 });
 
-console.log("PUBLIC:", process.env.IMAGEKIT_PUBLIC_KEY);
-console.log("PRIVATE:", process.env.IMAGEKIT_PRIVATE_KEY ? "EXISTS" : "MISSING");
-console.log("URL:", process.env.IMAGEKIT_URL_ENDPOINT);
 
 module.exports = imagekit;
